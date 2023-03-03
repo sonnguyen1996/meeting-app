9b308abd614067a69cbf40ef6020d01941123d95
Nguyen Thanh Son <nthanhson@vnpt.vn>
2023-03-03 17:31:19 +0700

    update model

---
+++ domain/src/main/java/com/demo/domain/domain/response/RoomResponse.kt
@@ -0,0 +1,30 @@
+package com.demo.domain.domain.response
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+open class RoomResponse : Serializable {
+    @SerializedName("roomId")
+    val roomId: String = ""
+
+    @SerializedName("customRoomId")
+    val customRoomId: String = ""
+
+    @SerializedName("userId")
+    val userId: String = ""
+
+    @SerializedName("disabled")
+    val disabled: Boolean = false
+
+    @SerializedName("createdAt")
+    val createdAt: String = ""
+
+    @SerializedName("updatedAt")
+    val updatedAt: String = ""
+
+    @SerializedName("id")
+    val id: String = ""
+
+    @SerializedName("links")
+    val links: LinkResponse? = null
+}
